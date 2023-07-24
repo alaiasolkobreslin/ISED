176d0fa827e645861d392987c761e8e209c657fd
Alaia Solko-Breslin <ajs644@cornell.edu>
2023-07-24 13:10:12 +0000

    segment update

--- generation-pipeline/unstructured/Coffee/segment.py
+++ generation-pipeline/unstructured/Coffee/segment.py
@@ -7,24 +7,67 @@ import torch
 import numpy as np
 import cv2
 
-DUMP_IMAGE_PATH = ".tmp/scallop-sam/"
+DUMP_IMAGE_PATH = ".tmp/sample-sam-generated-masks/rust/"
+
+
+def segment_anything_bboxes(
+        img: torch.Tensor,
+        img_name: str,
+        dump_image: bool = True,
+):
+    sam_config.configure_sam([])
+    sam = sam_config.get_sam_model()
+    if sam is not None:
+        from segment_anything import SamAutomaticMaskGenerator
+        mask_generator = SamAutomaticMaskGenerator(sam)
+
+        img = torch.transpose(input=img, dim0=0, dim1=1)
+        img = torch.transpose(input=img, dim0=1, dim1=2)
+
+        cvt_img = torch.stack(
+            [img[:, :, 2], img[:, :, 1], img[:, :, 0]], dim=2)  # BGR to RGB
+        np_image = cvt_img.numpy()
+
+        masks = mask_generator.generate(np_image)
+        bboxes_areas = []
+        bbox_dicts = []
+        for i, mask in enumerate(masks):
+
+            np_mask = torch.from_numpy(mask["segmentation"])
+            if dump_image:
+                save_temporary_image(img_name, i, "mask", np_mask)
+
+            area = mask["area"]
+            bbox = mask["bbox"]
+            xmin, ymin = bbox[0], bbox[1]
+            w, h = bbox[2], bbox[3]
+            bbox_dict = {
+                "area": area,
+                "bbox": {
+                    "xmin": xmin,
+                    "ymin": ymin,
+                    "xmax": w + xmin,
+                    "ymax": h + ymin,
+                }
+            }
+            bbox_dicts.append(bbox_dict)
+
+            # xmin, ymin = bbox[0], bbox[1]
+            # w, h = bbox[2], bbox[3]
+            # bbox_xyxy = [xmin, ymin, w + xmin, h + ymin]
+            # bboxes_areas.append((area, bbox_xyxy))
+        return bbox_dicts
 
 
 def segment_anything(
-    # item,
-    # fields: List[str],
-    # *,
     img: torch.Tensor,
-    xmin: int,
-    ymin: int,
-    xmax: int,
-    ymax: int,
+    img_name: str,
+    bboxes: List[List[int]],
+    # xmin: int,
+    # ymin: int,
+    # xmax: int,
+    # ymax: int,
     dump_image: bool = True,
-    iou_threshold: float = 0.88,
-    area_threshold: int = 1000,
-    # limit: Optional[int] = None,
-    expand_crop_region: int = 0,
-    debug: bool = False,
 ):
 
     sam_config.configure_sam([])
@@ -40,26 +83,30 @@ def segment_anything(
         np_image = cvt_img.numpy()
         predictor.set_image(np_image)
 
-        input_box = np.array([xmin, ymin, xmax, ymax])
+        total_area = 0
+        for i, bbox in enumerate(bboxes):
+            input_box = np.array(bbox)
 
-        masks, iou_preds, _ = predictor.predict(
-            point_coords=None,
-            point_labels=None,
-            box=input_box[None, :],
-            multimask_output=None,
-        )
+            masks, iou_preds, _ = predictor.predict(
+                point_coords=None,
+                point_labels=None,
+                box=input_box[None, :],
+                multimask_output=None,
+            )
 
-        masks_data = torch.from_numpy(masks)
-        rle = mask_to_rle_pytorch(masks_data)[0]
-        areas = area_from_rle(rle)
+            masks_data = torch.from_numpy(masks)
+            rle = mask_to_rle_pytorch(masks_data)[0]
+            areas = area_from_rle(rle)
 
-        # print(f"bbox area: {areas}")
+            # np_mask = torch.from_numpy(mask)
+            mask = masks[0]
+            np_mask = torch.from_numpy(mask)
+            if dump_image:
+                save_temporary_image(img_name, i, "mask", np_mask)
 
-        mask = torch.from_numpy(masks[0])
-        if dump_image:
-            save_temporary_image(id(img), 0, "mask", mask)
+            total_area += areas
 
-        yield (areas, mask)
+        return total_area
 
 
 def mask_to_rle_pytorch(tensor) -> List[Dict[str, Any]]:
