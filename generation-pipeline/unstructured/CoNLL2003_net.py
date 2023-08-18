fix spot where softmax is used

generation-pipeline/unstructured/CoNLL2003_net.py
@@ -17,4 +17,4 @@ class BertModel(torch.nn.Module):
         output = self.bert(input_ids=input_id,
                            attention_mask=mask, return_dict=False)
 
-        return output
+        return torch.softmax(input=output[0], dim=2)