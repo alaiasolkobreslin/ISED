f56921fa1085136952d2af64e0ffa9a2e7eb8960
Alaia Solko-Breslin <ajs644@cornell.edu>
2024-04-24 15:59:17 +0000

    more cleanup

---
+++ baselines/dpl/mnist/equal.py
@@ -0,0 +1,68 @@
+from json import dumps
+
+import torch
+
+from deepproblog.dataset import DataLoader
+from deepproblog.engines import ApproximateEngine, ExactEngine
+from deepproblog.evaluate import get_confusion_matrix
+from deepproblog.examples.MNIST.data import MNIST_train, MNIST_test, MNISTOperator
+from deepproblog.examples.MNIST.network import MNIST_Net
+from deepproblog.model import Model
+from deepproblog.network import Network
+from deepproblog.train import train_model
+
+from argparse import ArgumentParser
+    
+def equal(n: int, dataset: str, seed=None):
+    return MNISTOperator(
+        dataset_name=dataset,
+        function_name="equal",
+        operator=lambda x: 1 if x[0] == x[1] else 0, #define solution here
+        size=n,
+        arity=2,
+        seed=seed,
+    )
+
+
+parser = ArgumentParser("equal")
+parser.add_argument("--seed", type=int, default=1234)
+args = parser.parse_args()
+
+method = "exact"
+N = 1
+
+name = "equal_test{}_{}_{}".format(method, N, args.seed)
+
+train_set = equal(N, "train")
+test_set = equal(N, "test")
+
+train_set = train_set.subset(0, 10000)
+test_set = test_set.subset(0, 1000)
+
+network = MNIST_Net()
+
+pretrain = 0
+if pretrain is not None and pretrain > 0:
+    network.load_state_dict(torch.load("models/pretrained/all_{}.pth".format(pretrain)))
+net = Network(network, "mnist_net", batching=True)
+net.optimizer = torch.optim.Adam(network.parameters(), lr=1e-3)
+
+model = Model("models/equal.pl", [net])
+if method == "exact":
+    model.set_engine(ExactEngine(model), cache=True)
+elif method == "geometric_mean":
+    model.set_engine(
+        ApproximateEngine(model, 1, ApproximateEngine.geometric_mean, exploration=False)
+    )
+
+model.add_tensor_source("train", MNIST_train)
+model.add_tensor_source("test", MNIST_test)
+
+loader = DataLoader(train_set, 2, False, seed=args.seed)
+train = train_model(model, loader, 10, log_iter=100, profile=0)
+model.save_state("snapshot/" + name + ".pth")
+train.logger.comment(dumps(model.get_hyperparameters()))
+train.logger.comment(
+    "Accuracy {}".format(get_confusion_matrix(model, test_set, verbose=1).accuracy())
+)
+train.logger.write_to_file("log/" + name)
