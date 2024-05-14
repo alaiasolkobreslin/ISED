6ace4c8e34f289d8c71c3333ec759c33435ec9fa
Alaia Solko-Breslin <ajs644@cornell.edu>
2024-05-14 15:54:32 +0000

    another nasr result

--- baselines/nasr/mnist/runner.sh
+++ baselines/nasr/mnist/runner.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Define a list of random seeds
-seeds=(3177 5848 9175 8725 1234 1357 2468 548 6787 8371)
+#seeds=(3177 5848 9175 8725 1234 1357 2468 548 6787 8371)
+seeds=(548 6787)
+# seeds=(8371)
 
 # Define a list of python files
-python_files=("sum_2.py" "sum_3.py" "sum_4.py" "not_3_4.py" "mult_2.py" "mod_2.py" "less_than.py" "how_many_3_4.py" "equal.py" "add_sub.py" "add_mod_3.py")
+# python_files=("sum_2.py" "sum_3.py" "sum_4.py" "not_3_4.py" "mult_2.py" "mod_2.py" "less_than.py" "how_many_3_4.py" "equal.py" "add_sub.py" "add_mod_3.py")
+python_files=("mult_2.py")
+# python_files=("not_3_4.py")
 
 # Create outputs directory if it doesn't exist
 mkdir -p outputs
